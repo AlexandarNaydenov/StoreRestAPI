b4a2c481f44c1cf24a36f30dadce1733dbf72d28
Александър Найденов <66973520+AlexandarNaydenov@users.noreply.github.com>
2020-07-08 11:17:26 +0300

    Update GetOneSecondHalfPrice.java

--- Store/Store/src/main/java/com/project/Store/GetOneSecondHalfPrice.java
+++ Store/Store/src/main/java/com/project/Store/GetOneSecondHalfPrice.java
@@ -1,14 +1,13 @@
-package com.project.Store;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class GetOneSecondHalfPrice extends Deal{
 
-    private HashMap<Product,Integer> quantity = new HashMap<Product,Integer>();
 
-    private void findQuantity(ArrayList<Product> products)
+    private HashMap<Product,Integer> findQuantity(ArrayList<Product> products)
     {
+        HashMap<Product,Integer> quantity = new HashMap<Product,Integer>();
         int i=0;
         if(products.size() >= 3) i = 3;
         for(;i< products.size();i++)
@@ -16,12 +15,13 @@ public class GetOneSecondHalfPrice extends Deal{
             quantity.putIfAbsent(products.get(i), 0);
             quantity.put(products.get(i), quantity.get(products.get(i)) + 1);
         }
+        return quantity;
     }
 
     @Override
     public int apply(int currentPrice,ArrayList<Product> products) {
+        HashMap<Product,Integer> quantity = findQuantity(products);
         int promotion = 0;
-        findQuantity(products);
         for (Product i : quantity.keySet()) {
             promotion += quantity.get(i)/2*i.getPrice()/2;
         }
